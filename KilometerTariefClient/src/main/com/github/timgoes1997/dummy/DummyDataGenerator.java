cb6fcf4558978d44ab1185e01a2410859ba0e1c5
Tim Goes <Tim_Goes4@hotmail.nl>
2018-06-08 11:34:51 +0200

    Added some testdata in dummydatagenerator

--- KilometerTariefClient/src/main/com/github/timgoes1997/dummy/DummyDataGenerator.java
+++ KilometerTariefClient/src/main/com/github/timgoes1997/dummy/DummyDataGenerator.java
@@ -3,6 +3,7 @@ package com.github.timgoes1997.dummy;
 import com.github.timgoes1997.entities.Region;
 import com.github.timgoes1997.entities.RegionRate;
 import com.github.timgoes1997.entities.enums.EnergyLabel;
+import com.github.timgoes1997.entities.enums.VehicleType;
 
 import java.math.BigDecimal;
 import java.time.DayOfWeek;
@@ -14,7 +15,7 @@ public class DummyDataGenerator {
     private List<Region> regionList;
     private List<RegionRate> regionRates;
 
-    public DummyDataGenerator(){
+    public DummyDataGenerator() {
         generate();
     }
 
@@ -29,16 +30,33 @@ public class DummyDataGenerator {
         regionList.add(new Region("region 5"));
         regionList.add(new Region("region 6"));
         regionList.add(new Region("region 7"));
-        regionRates.addAll(generateRegionWeek(regionList.get(0), new BigDecimal(0.1d), EnergyLabel.A));
+        BigDecimal carStart = new BigDecimal(0.03d);
+        BigDecimal carSteps = new BigDecimal(0.02);
+        BigDecimal truckStart = new BigDecimal(0.07d);
+        BigDecimal truckSteps = new BigDecimal(0.05d);
 
-
-
+        regionList.forEach(region -> {
+            generateRegionRateWeekDaysEnergyLabel(region, VehicleType.CAR, carStart, carSteps);
+        });
+        regionList.forEach(region -> {
+            regionRates.addAll(generateRegionRateWeekDaysEnergyLabel(region, VehicleType.TRUCK, truckStart, truckSteps));
+        });
     }
 
-    private List<RegionRate> generateRegionWeek(Region region, BigDecimal price, EnergyLabel label){
+    private List<RegionRate> generateRegionRateWeekDaysEnergyLabel(Region region, VehicleType vehicleType, BigDecimal startPrice, BigDecimal steps) {
         List<RegionRate> regionRates = new ArrayList<>();
-        for(DayOfWeek day : DayOfWeek.values()){
-            regionRates.add(new RegionRate(region, price, label, day, 0, 0, 23, 59));
+        BigDecimal currentPrice = startPrice;
+        for (EnergyLabel label : EnergyLabel.values()) {
+            regionRates.addAll(generateRegionRateWeekDays(region, vehicleType, currentPrice, label));
+            currentPrice.add(steps);
+        }
+        return regionRates;
+    }
+
+    private List<RegionRate> generateRegionRateWeekDays(Region region, VehicleType vehicleType, BigDecimal price, EnergyLabel label) {
+        List<RegionRate> regionRates = new ArrayList<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            regionRates.add(new RegionRate(region, vehicleType, price, label, day, 0, 0, 23, 59));
         }
         return regionRates;
     }
